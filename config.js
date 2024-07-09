440d2cc8e1702fc6de76afc72de19be2150fe065
hasan6227 <haral6227@gmail.com>
2024-07-09 08:00:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaFo1Vy2ZjCsyxfNFb09'
+global.linkGroup = 'https://whatsapp.com/channel/0029VaFo1Vy2ZjCsyxfNFb09'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "M Hassan Ramzan Haral";
