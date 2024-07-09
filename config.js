982094744af6cf70f742501e36be19654adf6871
hasan6227 <haral6227@gmail.com>
2024-07-09 07:59:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,25 +7,25 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "M Hassan Ramzan Haral";
+global.owner = ['923336227405'];
+global.premium = ['923057732219'];
+global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋 WhatsApp bot by M Hassan Ramzan Haral𝚢";
+global.author = "M Hassan Ramzan Haral";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
 global.autoread = false
-global.available = false
+global.available = true
 global.autoTyping = false
 global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
